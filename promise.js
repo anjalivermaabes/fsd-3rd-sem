f77c093aeaa6906e4ebc56d85b92378cb96ec84c
anjalivermaabes <anjv12345678@gmail.com>
2026-09-21 09:36:58 +0530

    revision

--- promise.js
+++ promise.js
@@ -1,83 +1,11 @@
-Promise((resolve, reject) => {
-    const promise1 = new Promise((resolve, reject) => {
-        success=false;
-        if(success){
-            resolve({
-                id:2503203100029,
-                username: "Archita Tripathi",
-            });
-        }   else{   
-            reject(new Error("data not fetched"));
-        }
-    });
-    // promise2
-    // .then((response) => {
-    //     console.log(response);
-    // })
-    // .catch((error) => {
-    //     console.log(error.message);
-    // });
-    const promise2 = new Promise((resolve, reject) => {
-        success=false;
-        if(success){
-            resolve({
-                id:250320310,
-                username: "Adhya Singh",
-            });
-        }   else{   
-            reject(new Error("data is not available"));
-        }
-    });
-    
-    // promise2
-    // .then((response) => {
-    //     console.log(response);
-    // })
-    // .catch((error) => {
-    //     console.log(error.message);
-    // });
-    Promise.all([promise1, promise2])
-    .then((response) => {
-        console.log(response);
-    })
-    .catch((error) => {
-        console.log(error.message);
-    });
-})
-Promise.race([promise1, promise2])
-.then((response) => {
-    console.log(response);
-})
-.catch((error) => {
-    console.log(error.message);
+const promise1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("Promise 1 resolved");
+    }, 1000);
 });
-Promise.allSettled([promise1, promise2])
-.then((response) => {
-    console.log(response);      
-})
-.catch((error) => {
-    console.log(error.message);
-})
-promise1
-.then((response) => {
-    console.log(response);
-})
-.then((response2) => {
-    console.log("Promise resolved");
-} )
-.catch((error) => {
-    console.log(error.message);
-}   )             
-function fetchuserDData() {
-    return new Promise((resolve, reject) => {
-        let success = true;
-        if (success) {
-            resolve({       
-                id: 101,
-                username: "Diya"
-            });
-        } else {
-            reject(new Error("User data not fetched"));
-        } 
-    });
-}
+
+const promise2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("Promise 2 resolved");
+    }, 2000);
+}); 
